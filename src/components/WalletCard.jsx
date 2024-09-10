5f04fd439e84e2e6913df3e4d45d427fe433c718
Sanamdeep Kajla <sanamdeep.k@codebrewinnovations.com>
2024-09-10 17:56:12 +0530

    fixed NaN issue

--- src/components/WalletCard.jsx
+++ src/components/WalletCard.jsx
@@ -19,8 +19,8 @@ function WalletCard({
                             alt="" />
                         <div>
                             <h5 class="card-title text-start">{name}</h5>
-                            <h6 class="card-subtitle mb-2 text-start">{balance && Number(balance).toFixed(3)} {symbol}</h6>
-                            <h6 class="card-text mb-2 text-start">${balance && Number(balance * conversionRate).toFixed(3)} USD</h6>
+                            <h6 class="card-subtitle mb-2 text-start">{+balance > 0 ? Number(balance).toFixed(3) : 0} {symbol}</h6>
+                            <h6 class="card-text mb-2 text-start">${+(balance > 0 && conversionRate > 0) ? Number(balance * conversionRate).toFixed(3) : 0} USD</h6>
                         </div>
                     </div>
                 </div>
